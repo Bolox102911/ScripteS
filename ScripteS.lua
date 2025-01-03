4e6a38a33b069e153792fb1cba0c21dd5aaa706f
Gabriel <korzynscysg@gmail.com>
2025-01-03 18:55:51 +0100

    gv

--- ScripteS.lua
+++ ScripteS.lua
@@ -6,10 +6,18 @@ local games = {
     [15277359883] = "https://raw.githubusercontent.com/Bolox102911/ScripteS/refs/heads/main/scripts/GrassCuttingSim/Script.lua"
 }
 
-for ids, url in next, games do
-    if table.find(ids, game.PlaceId) then
-        Notification.Notify("ScripteS", "Loading ...","rbxassetid://4483345998");
+local foundGame = false
+
+for id, url in pairs(games) do
+    if game.PlaceId == id then
+        foundGame = true
+        Notification.Notify("ScripteS Hub", "Loading ...", "rbxassetid://4483345998")
         loadstring(game:HttpGet(url))()
         break
     end
+end
+
+if not foundGame then
+    local currentPlaceId = game.PlaceId
+    Notification.Notify("ScripteS Hub", "Wrong game. Your current game ID is: " .. tostring(currentPlaceId), "rbxassetid://4483345998")
 end
