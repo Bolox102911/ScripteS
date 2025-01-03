9aaf892c5440e36ab09ba848507e6bb93400c8d6
Gabriel <korzynscysg@gmail.com>
2025-01-03 19:02:34 +0100

    deleted

--- scripts/GrassCuttingSim/Script.lua
+++ scripts/GrassCuttingSim/Script.lua
@@ -14,9 +14,6 @@ local Window = Library:CreateWindow({
     MenuFadeTime = 0
 })
 
-    shared.NotifyStyle = "Linoria"
-    shared.NotifyVolume = 2
-    shared.Notify = require("Notify")
 
 -- Add Tabs
 local Tabs = {
